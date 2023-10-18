516ed000eca420527a85d8cc53b57d525ae18cd0
DuaaRabie <Duaarabie11@gmail.com>
2023-10-17 17:41:26 -0700

    update code

--- built_in.c
+++ built_in.c
@@ -18,10 +18,12 @@ int built_cmd(char **argv, char *cmd)
 	free(copy);
 
 	if (argv[0][0] == 'e' && argv[0][1] == 'x'
-			&& argv[0][2] == 'i' && argv[0][3] == 't')
+			&& argv[0][2] == 'i' && argv[0][3] == 't'
+			&& argv[0][4] == '\0')
 		exit_fun(argv, cmd);
 
-	if (argv[0][0] == 'e' && argv[0][1] == 'n' && argv[0][2] == 'v')
+	if (argv[0][0] == 'e' && argv[0][1] == 'n' && argv[0][2] == 'v'
+			&& argv[0][3] == '\0')
 	{
 		free_all(argv, cmd, NULL);
 		_env();
