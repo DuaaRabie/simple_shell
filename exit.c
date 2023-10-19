66d418c9621c2c7814107a82de75311a0eb74a04
DuaaRabie <Duaarabie11@gmail.com>
2023-10-19 08:47:34 -0700

    update second deadline

--- exit.c
+++ exit.c
@@ -18,22 +18,20 @@ void exit_fun(char **argv, char *cmd, int *status)
 	while (argv[1])
 	{
 		if (argv[1][i] >= '0' && argv[1][i] <= '9')
-		{
-			exit_value = (exit_value * 10) + (argv[1][i] - 48);
-			i++;
-		}
+			exit_value = (exit_value * 10) + (argv[1][i++] - 48);
 		else
 		{
 			if (argv[1][i] == '\n' || argv[1][i] == '\0')
 				break;
-			write(1, "./hsh: exit: ", 13);
 			if (isatty(STDIN_FILENO))
 			{
+				write(1, "./hsh: exit: ", 13);
 				write(1, argv[1], _strlen(argv[1]));
 				write(1, not_num, _strlen(not_num));
 			}
 			else
 			{
+				write(1, "./hsh: 1: exit: ", 16 );
 				write(1, not_num2, _strlen(not_num2));
 				write(1, ": ", 2);
 				write(1, argv[1], _strlen(argv[1]));
