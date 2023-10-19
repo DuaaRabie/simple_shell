720a8e22622e9f59aa24b9b783fbc39473138a58
DuaaRabie <Duaarabie11@gmail.com>
2023-10-19 09:32:17 -0700

    update second deadline

--- print_error.c
+++ print_error.c
@@ -14,24 +14,24 @@ void print_error(char ch, char **argv, char **av)
 
 	if (isatty(STDIN_FILENO))
 	{
-		write(1, av[0], _strlen(av[0]));
-		write(1, ": ", 2);
-		write(1, argv[0], _strlen(argv[0]));
+		write(2, av[0], _strlen(av[0]));
+		write(2, ": ", 2);
+		write(2, argv[0], _strlen(argv[0]));
 		switch (ch)
 		{
 			case 'c':
-				write(1, c, _strlen(c));
+				write(2, c, _strlen(c));
 				break;
 			case 'd':
-				write(1, d, _strlen(d));
+				write(2, d, _strlen(d));
 				break;
 		}
 	}
 	else
 	{
-		 write(1, av[0], _strlen(av[0]));
-		 write(1, ": 1: ", 5);
-		 write(1, argv[0], _strlen(argv[0]));
-		 write(1, ": not found\n", 12);
+		 write(2, av[0], _strlen(av[0]));
+		 write(2, ": 1: ", 5);
+		 write(2, argv[0], _strlen(argv[0]));
+		 write(2, ": not found\n", 12);
 	}
 }
